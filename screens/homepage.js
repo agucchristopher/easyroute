5586e2dd810b9af35539fd7efaf3085c9ef9332d
Agu C. Christopher <83035789+dev-kay1@users.noreply.github.com>
2022-08-28 08:45:51 -0700

    Maps Update

--- screens/homepage.js
+++ screens/homepage.js
@@ -1,44 +1,66 @@
-import { Text, View, Dimensions, StyleSheet, ScrollView, FlatList, Alert } from "react-native";
-import { StackNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Header from "../components/Header";
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { StackNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { useState } from "react";
-import Statusbar from 'expo-status-bar'
+import Statusbar from "expo-status-bar";
 export default function Homepage({ navigation }) {
-    const [name, setName] = useState('');
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
-    return (
+  const [name, setName] = useState("");
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
+  return (
+    <ScrollView>
+      <View>
+        <View style={{ height: 50 }}>
+          <TouchableOpacity
+            style={{
+              zIndex: 3,
+              padding: 10,
+              backgroundColor: "transparent",
+            }}
+          >
+            <Icon name="bars" size={30} color="black" />
+          </TouchableOpacity>
+        </View>
         <ScrollView>
-            <View>
-                <Header />
-                <ScrollView>
-                    <MapView 
-                     initialRegion={{
-                        latitude: 6.9746,
-                        longitude:  4.8472,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                      }}
-                    provider={PROVIDER_GOOGLE}
-                    style={{height: windowHeight, alignSelf: 'stretch', width: windowWidth,}} />
-                    <Text>hi</Text>
-                </ScrollView>
-            </View>
+          <MapView
+            initialRegion={{
+              latitude: 6.9746,
+              longitude: 4.8472,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            provider={PROVIDER_GOOGLE}
+            style={{
+              height: windowHeight,
+              alignSelf: "stretch",
+              width: windowWidth,
+              zIndex: 1,
+            }}
+          />
         </ScrollView>
-
-    );
+      </View>
+    </ScrollView>
+  );
 }
 const styles = StyleSheet.create({
-    introtext: {
-        textAlign: 'center',
-        fontSize: 30,
-        fontFamily: 'monospace',
-        color: 'white',
-    },
-    intro: {
-        marginTop: 0,
-        backgroundColor: '#1C62CA',
-    }
-})
+  introtext: {
+    textAlign: "center",
+    fontSize: 30,
+    fontFamily: "monospace",
+    color: "white",
+  },
+  intro: {
+    marginTop: 0,
+    backgroundColor: "#1C62CA",
+  },
+});
